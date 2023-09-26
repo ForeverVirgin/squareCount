d38c9fdbbab4c182040eba2d1932d5f27671abd2
ForeverVirgin <67623296+ForeverVirgin@users.noreply.github.com>
2023-09-26 15:57:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     packages=find_packages(include=['squareCount']),
     version='0.0.1',
     description='Counting squares of circles, triangles, etc.',
-    author='WildTale',
-    author_email='hvostulitki@gmail.com',
     url='',
     license='MIT',
     install_requires=[],
